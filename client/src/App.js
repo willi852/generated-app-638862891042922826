9cd5e9162deb8ab948a008fe630dedc2b6648049
App Generator <noreply@appgenerator.com>
2025-06-23 15:25:15 -0500

    Add client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Scientific Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
